d13d9be800402368a7a0e2a6fab1bac4858349ee
Mulki Mohamed <mulkim@uw.edu>
2019-02-25 17:43:21 -0800

    Updates

---
+++ precache-manifest.6be4eec860caf80eabe4e3b3d6fb240d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "56a19ee082faf18ab324",
+    "url": "/roots/static/js/runtime~main.56a19ee0.js"
+  },
+  {
+    "revision": "0b32b636f86d75e0c9b7",
+    "url": "/roots/static/js/main.0b32b636.chunk.js"
+  },
+  {
+    "revision": "5498041711a0aba949ba",
+    "url": "/roots/static/js/2.54980417.chunk.js"
+  },
+  {
+    "revision": "0b32b636f86d75e0c9b7",
+    "url": "/roots/static/css/main.19cd9e64.chunk.css"
+  },
+  {
+    "revision": "ddd4c203a9c7e5da3dc37542059196d2",
+    "url": "/roots/index.html"
+  }
+];
