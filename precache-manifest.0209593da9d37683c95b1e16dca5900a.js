26cd8e7a593ea1add87e04d24a671b617462a26c
Mulki Mohamed <mulkim@uw.edu>
2019-03-05 23:07:56 -0800

    Updates

---
+++ precache-manifest.0209593da9d37683c95b1e16dca5900a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbd32badf3dd0c7879451e607637b4b0",
+    "url": "/roots/static/media/Nature.cbd32bad.jpg"
+  },
+  {
+    "revision": "56a19ee082faf18ab324",
+    "url": "/roots/static/js/runtime~main.56a19ee0.js"
+  },
+  {
+    "revision": "f7e3ba76728b5651ed55",
+    "url": "/roots/static/js/main.f7e3ba76.chunk.js"
+  },
+  {
+    "revision": "56b9a17e329c1b36d1c6",
+    "url": "/roots/static/js/2.56b9a17e.chunk.js"
+  },
+  {
+    "revision": "f7e3ba76728b5651ed55",
+    "url": "/roots/static/css/main.19cd9e64.chunk.css"
+  },
+  {
+    "revision": "6e1c64be93269ae0c29068ad7deacb0b",
+    "url": "/roots/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/roots/precache-manifest.cc48871eeabd8dc23422dfe6d04af13b.js"
+  "/roots/precache-manifest.0209593da9d37683c95b1e16dca5900a.js"
 );
 
 workbox.clientsClaim();
